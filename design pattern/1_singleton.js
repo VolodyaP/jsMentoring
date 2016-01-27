b6bc8f107f46fef9b19e681810cd29a5b8c60e79
Panchishin Volodimir <weber.uk.lv@gmail.com>
2016-01-27 13:07:36 +0200

    Patterns update

--- design pattern/1_singleton.js
+++ design pattern/1_singleton.js
@@ -2,6 +2,17 @@
 * Singleton design pattern
 */
 
-var obj = {
-    name : 'singleton'
-};
+function Singleton() {
+
+    if (typeof Singleton.instance === "object") {
+        return Singleton.instance;
+    }
+
+    Singleton.instance = this;
+}
+
+    var obj1 = new Singleton();
+    var obj2 = new Singleton();
+    var res = obj1 === obj2;
+
+    console.log(res);
